efeba698f3815b50e2bd2f7615ed3a624fa6ed11
Fatih <fatihcihan666@gmail.com>
2023-12-22 20:09:02 +0300

    push and shift methods were used

--- methods/index.js
+++ methods/index.js
@@ -1,1 +1,16 @@
-console.log('hello world ^^');
+// Array Methods;
+const arr = [1, 2, 3, 4, 5, 6, 7];
+const arr1 = [8, 9, 10];
+
+
+/* // push -> adds element to array
+arr.push(8);
+console.log(arr);   */
+
+
+/* // shift -> deletes the first element of the array
+arr.shift();
+console.log(arr); */
+
+
+
