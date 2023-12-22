4f6d866e98ef161134640e12011b863874ac04b4
Fatih <fatihcihan666@gmail.com>
2023-12-22 21:53:38 +0300

    examples of slice, concat, spread were made

--- methods/index.js
+++ methods/index.js
@@ -13,4 +13,25 @@ arr.shift();
 console.log(arr); */
 
 
+/* // slice -> returns selected elements in an array as A NEW ARRAY
+const newArr = arr.slice(2,6);      // [ 3, 4, 5, 6 ]
+console.log(newArr); */
+
+
+/* // concat -> does the merging 
+let newArr = arr.concat(arr1);
+console.log(newArr);    // [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] */
+
+/* // spread -> ... 
+arr.push(...arr1);
+console.log(arr);        // [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10  */
+
+
+
+
+
+
+
+
+
 
