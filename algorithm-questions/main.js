700f064929b2e6a8a85b4953fe400972a0caed9e
Fatih <fatihcihan666@gmail.com>
2024-02-26 18:22:58 +0300

    implement a solution to find the progression by subtracting each other

---
+++ algorithm-questions/main.js
@@ -0,0 +1,15 @@
+// 1) [8,4,6,2,3] => output => [4,2,4,2,3] ---> the method of progress by subtracting from each other
+
+const solution = (arr) => {
+    return arr.map((_, i) => {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] > arr[j]) {
+                arr[i] = arr[i] - arr[j];
+                break;
+            }
+        }
+        return arr[i];
+    })
+}
+
+console.log(solution([8, 4, 6, 2, 3]));
