63e95baa68d77e3fbd4976c3e8ffc2d394f236a5
Fatih <fatihcihan666@gmail.com>
2024-02-28 20:18:21 +0300

    finding whether elements in different arrays are negative

--- algorithm-questions/main.js
+++ algorithm-questions/main.js
@@ -1,6 +1,6 @@
 // 1) [8,4,6,2,3] => output => [4,2,4,2,3] ---> the method of progress by subtracting from each other
 
-const solution = (arr) => {
+/* const solution = (arr) => {
     return arr.map((_, i) => {
         for (let j = i + 1; j < arr.length; j++) {
             if (arr[i] > arr[j]) {
@@ -12,4 +12,21 @@ const solution = (arr) => {
     })
 }
 
-console.log(solution([8, 4, 6, 2, 3]));
+console.log(solution([8, 4, 6, 2, 3])); */
+
+
+// 2) input [[4,3,2,-1], [3,-2,-1,6], [5,-3,0,-1]]   ---> how many negative values
+
+const solution = (arr) => {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            if (arr[i][j] < 0) {
+                newArr.push(arr[i][j])
+            }
+        }
+    }
+    return newArr.length;
+}
+
+console.log(solution([4, 3, 2, -1], [3, -2, -1, 6], [5, -4, 0, -1]));
